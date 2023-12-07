312b96a453cb588ea32670eef702bad239e9511d
Renat Mardanov <rent1995@ya.ru>
2023-12-07 19:14:42 +0700

    refactoring HomePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ReactElement, ReactNode, useState } from "react";
 import { GameField, GameInfo, GameTitle } from "../components/game";
 import { Header } from "../components/header";
 import { useGameState } from "../hooks";
@@ -20,62 +20,71 @@ export default function HomePages() {
     playersCount,
   });
   return (
-    <div className="bg-slate-50 min-h-screen">
-      <Header />
-      <main className="pt-6 mx-auto w-max">
-        <GameTitle playersCount={playersCount} />
-        <GameInfo
-          playersCount={playersCount}
-          className="mt-4"
-          currentStep={gameState.currentStep}
-          isWinner={!!winnerSymbol}
-          handlePlayerTimeOver={() =>
-            handlePlayerTimeOver(gameState.currentStep)
-          }
-        />
+    <HomePageLayout header={<Header />}>
+      <GameTitle playersCount={playersCount} />
+      <GameInfo
+        playersCount={playersCount}
+        className="mt-4"
+        currentStep={gameState.currentStep}
+        isWinner={!!winnerSymbol}
+        handlePlayerTimeOver={() => handlePlayerTimeOver(gameState.currentStep)}
+      />
 
-        {winnerSymbol && (
-          <div className="my-4 flex items-center justify-center">
-            {" "}
-            Победитель{"   "}
-            <GameSymbol symbol={winnerSymbol} className="" />
+      {winnerSymbol && (
+        <div className="my-4 flex items-center justify-center">
+          {" "}
+          Победитель{"   "}
+          <GameSymbol symbol={winnerSymbol} className="" />
+        </div>
+      )}
+
+      <UiModal
+        width="md"
+        className=""
+        isOpen={!!winnerSymbol}
+        onClose={() => {
+          console.log("close");
+        }}
+      >
+        <UiModal.Header className="">Игра завершена</UiModal.Header>
+        <UiModal.Body className="">
+          <div className="text-sm">
+            Победитель: <span className="text-teal-600">Renatka</span>
           </div>
-        )}
+          <div className="text-3xl">Обновляй, нихера не готово</div>
+        </UiModal.Body>
+        <UiModal.Footer className="">
+          <UiButton className="" variant="outline" size="md">
+            Вернуться
+          </UiButton>
+          <UiButton className="" variant="primary" size="md">
+            Играть снова
+          </UiButton>
+        </UiModal.Footer>
+      </UiModal>
 
-        <UiModal
-          width="md"
-          className=""
-          isOpen={!!winnerSymbol}
-          onClose={() => {
-            console.log("close");
-          }}
-        >
-          <UiModal.Header className="">Игра завершена</UiModal.Header>
-          <UiModal.Body className="">
-            <div className="text-sm">
-              Победитель: <span className="text-teal-600">Renatka</span>
-            </div>
-            <div className="text-3xl">Обновляй, нихера не готово</div>
-          </UiModal.Body>
-          <UiModal.Footer className="">
-            <UiButton className="" variant="outline" size="md">
-              Вернуться
-            </UiButton>
-            <UiButton className="" variant="primary" size="md">
-              Играть снова
-            </UiButton>
-          </UiModal.Footer>
-        </UiModal>
+      <GameField
+        gameState={gameState}
+        handleCellClick={handleCellClick}
+        nextStep={nextStep}
+        winnerSequence={winnerSequence}
+        winnerSymbol={winnerSymbol}
+        className="mt-6"
+      />
+    </HomePageLayout>
+  );
+}
 
-        <GameField
-          gameState={gameState}
-          handleCellClick={handleCellClick}
-          nextStep={nextStep}
-          winnerSequence={winnerSequence}
-          winnerSymbol={winnerSymbol}
-          className="mt-6"
-        />
-      </main>
+interface HomePageLayoutProps {
+  header: ReactElement;
+  children: ReactNode;
+}
+
+function HomePageLayout({ header, children }: HomePageLayoutProps) {
+  return (
+    <div className="bg-slate-50 min-h-screen">
+      {header}
+      <main className="pt-6 mx-auto w-max">{children}</main>
     </div>
   );
 }
